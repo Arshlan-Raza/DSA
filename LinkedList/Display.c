fbdc2965990e2e439eaf8c46b88b0d7d8d1d786a
Arshlan <arshalanraja@gmail.com>
2022-12-27 12:12:26 +0530

    Corrected code

--- LinkedList/Display.c
+++ LinkedList/Display.c
@@ -2,28 +2,41 @@
 #include<stdlib.h>
 #include<conio.h>
 
-// int display(struct node *);
-// int add(struct node *);
-
-struct node{
+struct Node {
     int data;
-    struct node *next;
-    
+    struct Node *next;
 };
+
+void display(struct Node *ptr){
+    while (ptr!=NULL){
+        printf("Element are :%d\n",ptr->data);
+        ptr = ptr->next;
+    }
+}
+
 int main(){
-    int data;
-    struct node *p;
-    p = (struct node *)malloc(sizeof(struct node));
-    while(data!=-1){
-        printf("Enter the data:");
-        scanf("%d",&data);
-        printf("to exit press -1\n");
-    }
-    
-    while(p!=NULL){
-        printf("%d",p->data);
-        p = p->next;
-    }
-    free(p);
-    
+    struct Node *head ;
+    struct Node *second;
+    struct Node *third ;
+    struct Node *fourth ;
+
+    head = (struct Node *)malloc (sizeof (struct Node));
+    second =  (struct Node *)malloc (sizeof (struct Node));
+    third = (struct Node *)malloc (sizeof (struct Node));
+    fourth = (struct Node *)malloc (sizeof (struct Node));
+
+    head->data = 1;
+    head->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = fourth;
+    fourth->data = 4;
+    fourth->next = NULL;
+    display(head);
+    free(head);
+    free(second);
+    free(third);
+    free(fourth);
+    return 0;
 }
